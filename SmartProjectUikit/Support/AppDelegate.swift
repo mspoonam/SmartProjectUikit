00bc7f5599c2e6ec0cb1ce4130772d8df9b5f825
mspoonam <mspoonam.learning@gmail.com>
2022-10-30 07:19:53 -0400

    mvvm: Created folders to represent mvvm architecture and folder structure

--- SmartProjectUikit/Support/AppDelegate.swift
+++ SmartProjectUikit/Support/AppDelegate.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
+// Entry Point of Project
+// Important Files
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
