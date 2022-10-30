90e0c972c72f173da85f9f0d03bf43629f34f5f5
mspoonam <mspoonam.learning@gmail.com>
2022-10-30 07:33:34 -0400

    added: Added files for empty folder & update info.plist file path in project settings

---
+++ SmartProjectUikit/Models/LoginRequestModel.swift
@@ -0,0 +1,12 @@
+//
+//  LoginRequestModel.swift
+//  SmartProjectUikit
+//
+//  Created by Poonam Pandey on 30/10/22.
+//
+
+import Foundation
+
+struct LoginRequest: Codable {
+    
+}
---
+++ SmartProjectUikit/View Models /LoginViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewModel.swift
+//  SmartProjectUikit
+//
+//  Created by Poonam Pandey on 30/10/22.
+//
+
+import Foundation
+
+final class LoginViewModel {
+    
+}
